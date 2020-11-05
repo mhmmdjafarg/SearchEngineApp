8bf0b9bafdded719e9de3abab5c2682c36d95f7d
Faris Hasim <farishasimsyauqi@gmail.com>
2020-11-05 23:27:33 +0700

    added fungsi.py

---
+++ fungsi.py
@@ -0,0 +1,105 @@
+def printdata(data):
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            print(data[i][j], end=" ")
+        print()
+
+
+def word_list_text(w, text):
+    for char in '-.,\n':
+        text = text.replace(char, ' ')
+    text = text.lower()
+
+    word_list = text.split()
+
+    for word in word_list :
+        w.append(word)
+    return w
+
+
+def wordlist(w, text):
+    for char in '-.,\n':
+        text = text.replace(char, ' ')
+    text = text.lower()
+
+    word_list = text.split()
+
+    for word in word_list :
+        if word not in w :
+            w.append(word)
+    return w
+
+
+def word_list_total(string_array):
+    w = []
+
+    for text in string_array:
+        w = wordlist(w, text)
+
+    return w
+
+
+def word_count(word, text):
+    w = []
+    w = word_list_text(w, text)
+    count = 0
+
+    for word_in_text in w:
+        if word_in_text == word:
+            count += 1
+
+    return count
+
+
+def panjang(a):
+    total = 0
+    for i in range(len(a)):
+        total += a[i]*a[i]
+    return total**0.5
+
+
+def dot_product(v, u):
+    # prekondisi : len(v) == len(u)
+    result = 0
+    for i in range(len(v)):
+        result += v[i]*u[i]
+    return result
+
+
+def similarity(v, u):
+    return dot_product(v, u)/(panjang(v)*panjang(u))
+
+
+string_array = ["bolu biru", "aku beli bolu biru makan bolu biru", "bolu warna biru"]
+
+word_list = word_list_total(string_array)
+
+print(word_list)
+
+word_data = [[0 for j in range(len(string_array))] for i in range(len(word_list))]
+
+for i in range(len(word_list)):
+    for j in range(len(string_array)):
+        word_data[i][j] = word_count(word_list[i], string_array[j])
+
+printdata(word_data)
+
+query = [word_data[i][0] for i in range(len(word_list))]
+ranks = [0 for i in range(len(string_array)-1)]
+array_of_sim = [0 for i in range(len(string_array)-1)]
+
+for j in range(1,len(string_array)):
+    word_vektor = [word_data[word][j] for word in range(len(word_list))]
+    sim = similarity(query,word_vektor)
+    idx = j
+    for i in range(j):
+        if sim > array_of_sim[i] :
+            temp = array_of_sim[i]
+            array_of_sim[i] = sim
+            sim = temp
+
+            temp = ranks[i]
+            ranks[i] = j
+            j = temp
+
+print(ranks)
