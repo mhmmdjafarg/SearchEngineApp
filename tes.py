7f7e95b1c8b2ec7f17b017c8eb354a35d84929e2
Muhammad Jafar <mjafarashadiq712@gmail.com>
2020-11-09 20:39:08 +0700

    update tes.py

--- tes.py
+++ tes.py
@@ -1,12 +1,13 @@
+from nltk.util import pr, print_string
 import fungsi
 import re
 
 sentence = "Welcome to run on running on server the Class Classes?, and some case Except that quality of life then revive survival welcome and coming throught with the woo."
+sentence = fungsi.removePunctuantion(sentence)
+print(sentence)
+
+
 listword = fungsi.removeStopwords(sentence)
-
-# text = fungsi.stemming(listword)
-# print(text)
-
-
-tes = fungsi.getFirstSentence(sentence)
-print(tes)
+print(listword)
+stem = fungsi.stemming(listword)
+print(stem)
