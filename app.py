d12fa5442c1863ae521eb4de117351bdf354dea2
Muhammad Jafar <mjafarashadiq712@gmail.com>
2020-11-13 01:00:33 +0700

    add some styling

--- app.py
+++ app.py
@@ -99,5 +99,9 @@ def openFile(filename):
     else:
         abort(404)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
   app.run(debug = True)
